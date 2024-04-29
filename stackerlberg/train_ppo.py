28edd5426ec307de299019604d5e92ac129a8ec2
YanzhenXiangRobotics <xyz000327@gmail.com>
2024-04-29 12:05:26 +0200

    ADD: refactored

--- stackerlberg/train_ppo.py
+++ stackerlberg/train_ppo.py
@@ -18,7 +18,7 @@ help="choose you environment: mat-game, maze-design. Default to maze-design")
 parser.add_argument("--headless", help="disable GUI", action="store_true")
 args = parser.parse_args()
 
-if __name__ == "__main__":
+def build_follower_env():
     if args.env == "mat-game":
         env_follower = FollowerWrapper(
             IteratedMatrixGame(matrix="prisoners_dilemma", episode_length=10, memory=2),
@@ -35,13 +35,25 @@ if __name__ == "__main__":
         )
     env_follower = SingleAgentFollowerWrapper(env_follower)
 
-    run = wandb.init(project="forl-stackerlberg", sync_tensorboard=True)
+    return env_follower
 
-    model = PPO("MlpPolicy", env_follower, verbose=1, tensorboard_log=f"runs/{run.id}")
-    model.learn(total_timesteps=50_000, callback=WandbCallback(gradient_save_freq=100,
-        model_save_path=f"models/{run.id}", verbose=2))
+def pretrain(env):
+    
+    run_follower = wandb.init(project="stackerlberg-follower", sync_tensorboard=True)
+    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=f"runs/{run_follower.id}")
+    model.learn(total_timesteps=50_000, callback=WandbCallback(gradient_save_freq=100, verbose=2))
     model.save("checkpoints/follower_ppo")
 
+    return model
+
+def test_pretrain(env, model):
+    for response in [[1,1,1,1,1], [0,0,0,0,0], [0,0,0,1,1]]:
+        for s in range(5):
+            obs = [s, *response]
+            action = model.predict(obs, deterministic=True)[0]
+            print(f"state: {s}, context: {response}, action: {action}")
+
+def build_leader_env(follower_model):
     if args.env == "mat-game":
         env_leader = FollowerWrapper(
             IteratedMatrixGame(matrix="prisoners_dilemma", episode_length=10, memory=2),
@@ -57,14 +69,23 @@ if __name__ == "__main__":
             num_queries=5,
         )
     env_leader = SingleAgentLeaderWrapper(
-        env_leader, queries=[0, 1, 2, 3, 4], follower_model=model
+        env_leader, queries=[0, 1, 2, 3, 4], follower_model=follower_model
     )
 
-    leader_model = PPO("MlpPolicy", env_leader, verbose=1)
-    leader_model.learn(total_timesteps=50_000)
+    return env_leader
+
+def train(env_leader):
+
+    run_leader = wandb.init(project="stackerlberg-leader", sync_tensorboard=True)
+    leader_model = PPO("MlpPolicy", env_leader, verbose=1, tensorboard_log=f"runs/{run_leader.id}")
+    leader_model.learn(total_timesteps=50_000, callback=WandbCallback(gradient_save_freq=100, verbose=2))
     leader_model.save("checkpoints/leader_ppo")
 
-    # play a single episode to check learned leader and follower policies
+    return leader_model
+
+def test_train(env_leader, leader_model):
+
+     # play a single episode to check learned leader and follower policies
     obs, _ = env_leader.reset()
     while True:
         action = leader_model.predict(obs, deterministic=True)[0]
@@ -74,3 +95,20 @@ if __name__ == "__main__":
 
         if terminated or truncated:
             break
+
+if __name__ == "__main__":
+
+    follower_env = build_follower_env()
+    follower_model = pretrain(env=follower_env)
+    test_pretrain(env=follower_env, model=follower_model)
+
+    leader_env = build_leader_env(follower_model=follower_model)
+    leader_model = train(env_leader=leader_env)
+    test_train(env_leader=leader_env, leader_model=leader_model)
+    
+
+    
+
+   
+
+   
