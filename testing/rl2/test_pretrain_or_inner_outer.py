dc3d793138c98158913123f9138cbca982893ef6
YanzhenXiangRobotics <xyz000327@gmail.com>
2024-06-05 12:51:04 +0200

    ADD: test rl2 inner-outer drone game

--- testing/rl2/test_pretrain_or_inner_outer.py
+++ testing/rl2/test_pretrain_or_inner_outer.py
@@ -5,7 +5,7 @@ Script for training stateful meta-reinforcement learning agents
 from utils.config_util import load_config_args_overwrite
 from utils.evaluate import evaluate
 
-def test_pretrain():
+def test():
     
     config = load_config_args_overwrite("configs/rl2.yml")
     
@@ -16,11 +16,11 @@ def test_pretrain():
             config,
             verbose=True,
             # leader_policy=[1, 1, 1, 1] + [np.random.randint(2) for _ in range(2**10-4)],
-            # leader_policy=[0 for _ in range(2**4)]
-            leader_policy= [0, 3, 0, 3, 3, 0, 3, 0, 0, 0, 3, 3, 0, 3, 0, 3]
+            leader_policy=[3 for _ in range(2**4)]
+            # leader_policy= [0, 3, 0, 3, 3, 0, 3, 0, 0, 0, 3, 3, 0, 3, 0, 3]
             # leader_policy= [1, 3, 1, 3, 3, 1, 3, 1, 1, 1, 3, 3, 1, 3, 1, 3]
         )
 
 
 if __name__ == "__main__":
-    test_pretrain()
+    test()
